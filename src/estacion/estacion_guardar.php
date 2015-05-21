8d8169276e9a1f7bc606ff9fb236347ee95a1cba
jesus dasilva <dasilvajm@canaima-hp.(none)>
2015-05-21 12:45:15 -0430

    creacion de formulario empresa 20 05 2015 en la noche

--- src/estacion/estacion_guardar.php
+++ src/estacion/estacion_guardar.php
@@ -1,26 +1,26 @@
 <?php
 
 /*
- *  EMPRESA GUARDAR NUEVO
+ *  ESTACIÓN GUARDAR NUEVO
  */
 
-$empresa->post('/empresa/guardar', function() use ($app) {
+$estacion->post('/estacion/guardar', function() use ($app) {
 
     try{
         
         //DATOS DEL FORMULARIO
-        $empresa = array('nombre'     => mb_strtoupper($app['request']->get('nombre'),'utf-8'),
+        $estacion = array('nombre'     => mb_strtoupper($app['request']->get('nombre'),'utf-8'),
                          'observacion'=> $app['request']->get('observacion'));
     
-        //BUSCAR NOMBRE DE EMPRESA
-        $buscarSql = " SELECT * FROM empresas WHERE nombre = ? ";
-        $nombreEncontrado = $app['db']->fetchAssoc($buscarSql, array($empresa['nombre']));
+        //BUSCAR NOMBRE DE ESTACIÓN
+        $buscarSql = " SELECT * FROM estaciones WHERE nombre = ? ";
+        $nombreEncontrado = $app['db']->fetchAssoc($buscarSql, array($estacion['nombre']));
     
         //VERIFICAR QUE NO ESTE REPETIDA
         if($nombreEncontrado) throw new Exception('Error, el nombre de la Empresa está repetido.'); 
         
-        //GUARDAR EMPRESA
-        $filasGuardadas = $app['db']->insert('empresas',$empresa); 
+        //GUARDAR ESTACIÓN
+        $filasGuardadas = $app['db']->insert('estaciones',$estacion); 
 									  
         //VERIFICAR QUE SE GUARDÓ
         if( $filasGuardadas <= 0 ) throw new Exception('Error al guardar la Empresa .');
@@ -29,7 +29,7 @@ $empresa->post('/empresa/guardar', function() use ($app) {
         $app['session']->getFlashBag()->add('success',array('message' => 'La Empresa fue creada con éxito'));
 		
         //REDIRECCIONAR A LISTAR
-        return $app->redirect($app['url_generator']->generate('empresaListar',array('pagina'=>0)));
+        return $app->redirect($app['url_generator']->generate('estacionListar',array('pagina'=>0)));
         
     //CAPTURAR LOS ERRORES    
     }catch (Exception $e) {
@@ -38,8 +38,8 @@ $empresa->post('/empresa/guardar', function() use ($app) {
         $app['session']->getFlashBag()->add('danger',array('message' => $e->getMessage()));
         
         //REGRESAR A LA PLANTILLA DE DATOS
-        return $app['twig']->render('empresa/empresa_datos.twig',array('empresa' => $empresa, 'editar'=> FALSE));
+        return $app['twig']->render('estacion/estacion_datos.twig',array('estacion' => $estacion, 'editar'=> FALSE));
     }
 
 })
-->bind('empresaGuardar');
+->bind('estacionGuardar');
--- src/estacion/estacion_nuevo.php
+++ src/estacion/estacion_nuevo.php
@@ -3,10 +3,11 @@
 /*
  *  ESTACION NUEVO
  */
+require_once 'estaciones_registros.php';
 
 $estacion->get('/estacion/nuevo', function() use ($app) {
 
-    $estacion = array('id' =>'','nombre'=> '','observacion'=>'');
+    $estacion = inicializarEstacion();
     
     //ABRIR FORMULARIO DE DATOS EN BLANCO
     return $app['twig']->render('estacion/estacion_datos.twig', array ('estacion' => $estacion,'editar' => FALSE));
