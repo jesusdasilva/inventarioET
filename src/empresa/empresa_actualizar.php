6573a936c008fa0a0c0e7207acfa9f5482027fdd
dasilvajm <dasilvajm@Equipo1>
2015-07-13 22:52:16 -0430

    actualizacion 13072015

--- src/empresa/empresa_actualizar.php
+++ src/empresa/empresa_actualizar.php
@@ -3,52 +3,50 @@
 /*
  *  EMPRESA ACTUALIZAR
  */
-
-
 $empresa->post('/empresa/actualizar', function() use ($app) {
 
     try{
-        
+
         //TOMAR LOS DATOS DEL FORMULARIO
         $empresa = array('id'          => $app['request']->get('id'),
                          'nombre'      => $app['request']->get('nombre'),
                          'observacion' => $app['request']->get('observacion'));
 
         //ACTUALIZAR
-        $filasActualizadas = $app['db']->update('empresas', 
-                                                array('observacion'=> $empresa['observacion']), 
+        $filasActualizadas = $app['db']->update('empresas',
+                                                array('observacion'=> $empresa['observacion']),
                                                 array('id'=>$empresa['id']));
-        
+
         //VERIFICAR QUE SE REALIZÓ LA ACTUALIZACIÓN
         if( $filasActualizadas <= 0 )
         {
-            
+
             //MENSAJE
             $app['session']->getFlashBag()->add('danger',array('message' => 'No se pudo actualizar la Empresa'));
-        
+
             //REGRESAR AL FORMULARIO DATOS
             return $app['twig']->render('empresa/empresa_datos.twig',array('empresa' => $empresa,'editar' => TRUE));
-            
+
         }else{
 
             //MENSAJE
             $app['session']->getFlashBag()->add('success',array('message' => 'Se actualizó con éxito la Empresa'));
-            
+
             //REDIRECCIONAR AL FORMULARIO LISTAR
             return $app->redirect($app['url_generator']->generate('empresaListar',array('pagina'=>0)));
-            
+
         }
-    
+
     //CAPTURAR ERROR
     }catch (Exception $e) {
-     
+
         //MENSAJE
         $app['session']->getFlashBag()->add('danger',array('message' => $e->getMessage()));
-        
+
         //MOSTRAR MENSAJE ERROR
-        return $app['twig']->render('mensaje_error.twig');        
-        
+        return $app['twig']->render('mensaje_error.twig');
+
     }
-	
+
 })
 ->bind('empresaActualizar');
--- src/empresa/empresa_guardar_nuevo.php
+++ src/empresa/empresa_guardar_nuevo.php
@@ -5,7 +5,7 @@
 $empresa->post('/empresa/guardar', function() use ($app) {
 
     try{
-        
+
         //DATOS DEL FORMULARIO
         $nombre = mb_strtoupper($app['request']->get('nombre'),'utf-8');//CAMBIAR A MAYÚSCULAS EL NOMBRE
         //VERIFICAR SI SE ENVIÓ EL CAMPO OBSERVACIÓ
@@ -18,42 +18,42 @@ $empresa->post('/empresa/guardar', function() use ($app) {
         //VERIFICAR SI EL NOMBRE ESTA REPETIDO
         $Sql = " SELECT * FROM vista_empresas WHERE nombre = ? ";
         $nombreEncontrado = $app['db']->fetchAssoc($Sql, array($nombre));
-            
+
         if(!$nombreEncontrado){//NO ESTA REPETIDO EL NOMBRE
-            
-            //INSERTAR         
+
+            //INSERTAR
             $registrosAfectados = $app['db']->insert('empresas',array('nombre' =>$nombre,
                                                                       'observacion'=>$observacion));
             if($registrosAfectados <= 0)
                 throw new Exception('Error, No se pudo crear la empresa.');
 
         }else{//NOMBRE DE EMPRESA REPETIDO
-            
+
             //MENSAJE
             $mensaje = 'La empresa se encuentra repetida';
             $app['session']->getFlashBag()->add('danger',array('message' => $message));
             //REENVIAR A FORMULÁRIO DATOS
             return $app['twig']->render('empresa/empresa_datos.twig',array('nombre' => $nombre,
-                                                                           'observacion' => $observacion, 
+                                                                           'observacion' => $observacion,
                                                                            'editar'=> TRUE));
         }
 
         //MENSAJE
-        $mensaje = 'La Empresa fue actualizada'; 
+        $mensaje = 'La Empresa fue actualizada';
         $app['session']->getFlashBag()->add('success',array('message' => $mensaje));
         //REDIRECCIONAR AL FORMULARIO LISTAR
         return $app->redirect($app['url_generator']->generate('empresaListar',array('pagina'=>0)));
-            
+
     //CAPTURAR ERROR
     }catch (Exception $e) {
-     
+
         //MENSAJE
         $app['session']->getFlashBag()->add('danger',array('message' => $e->getMessage()));
 
         //MOSTRAR MENSAJE ERROR
-        return $app['twig']->render('mensaje_error.twig');        
-        
+        return $app['twig']->render('mensaje_error.twig');
+
     }
-	
+
 })
 ->bind('empresaGuardarNuevo');
